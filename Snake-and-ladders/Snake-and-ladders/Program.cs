a8417962da841e7651f3247e1c9af34bca4f08ee
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-14 23:16:20 +0530

    Uc5 : Exact win position

--- Snake-and-ladders/Snake-and-ladders/Program.cs
+++ Snake-and-ladders/Snake-and-ladders/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Snake-and-ladders");
+   Console.WriteLine("Welcome to Snake-and-ladders");
 
 const int START = 0;
 const int FINISH = 100;
@@ -9,10 +9,13 @@ const int SNAKE = 2;
 Random random = new Random();
 
 int Roll_Dice()
+
 {
     return random.Next(1, 7);
 }
+
 int Player_Move_Option(int player_Roll_Dice)
+
 {
     int check_Player_Option = random.Next(0, 3);
     int player_Move = 0;
@@ -41,7 +44,10 @@ int Player_Move_Option(int player_Roll_Dice)
 
         // The Player then checks for a Option. 
         int player_move = Player_Move_Option(player_Roll_Dice);
-        player_Next_Position = player_position + player_move;
+        if (player_position + player_move > FINISH)
+            player_Next_Position = player_position;
+        else
+            player_Next_Position = player_position + player_move;
 
         if (player_Next_Position < START)
             player_position = START;
