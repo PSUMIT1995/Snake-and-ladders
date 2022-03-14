7635cd56a0a840ea4edd5a5683053590cb6fa208
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-14 23:58:07 +0530

    Uc6 : Number of times dice was played to win

--- Snake-and-ladders/Snake-and-ladders/Program.cs
+++ Snake-and-ladders/Snake-and-ladders/Program.cs
@@ -9,13 +9,10 @@ const int SNAKE = 2;
 Random random = new Random();
 
 int Roll_Dice()
-
 {
     return random.Next(1, 7);
 }
-
 int Player_Move_Option(int player_Roll_Dice)
-
 {
     int check_Player_Option = random.Next(0, 3);
     int player_Move = 0;
@@ -35,6 +32,7 @@ int Player_Move_Option(int player_Roll_Dice)
 
 {
     int player_position = 0, player_Next_Position;
+    int dice_roll_count = 0;
 
     //Repeats till the Player reaches the winning position 100.
     while (player_position < FINISH)
@@ -42,8 +40,12 @@ int Player_Move_Option(int player_Roll_Dice)
         // player rolls the dice and gets the value
         int player_Roll_Dice = Roll_Dice();
 
-        // The Player then checks for a Option. 
+        // number of times the dice was played is counted
+        dice_roll_count++;
+
+        // The Player then checks for a Option.
         int player_move = Player_Move_Option(player_Roll_Dice);
+
         if (player_position + player_move > FINISH)
             player_Next_Position = player_position;
         else
@@ -53,6 +55,11 @@ int Player_Move_Option(int player_Roll_Dice)
             player_position = START;
         else
             player_position = player_Next_Position;
+
+        // Player current position After rolling the Dice
+        Console.WriteLine("Player current position After rolling the Dice is " + player_position);
     }
-    Console.WriteLine("Player current position after the game is " + player_position);
+    Console.WriteLine();
+    Console.WriteLine("Player current position After the game is " + player_position);
+    Console.WriteLine("Total Number of Dice Roll in the game is " + dice_roll_count);
 }
